5842e787530571fa477c4446cfedab27be1a261c
Artem <ppjisd@gmail.com>
2026-05-25 08:19:09 +0300

    Auto-commit by Gradle

--- semester_3/web/laba_4/backend/src/main/java/com/yarukov/backend/controller/AuthController.java
+++ semester_3/web/laba_4/backend/src/main/java/com/yarukov/backend/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.yarukov.backend.dto.AuthRequest;
 import com.yarukov.backend.model.User;
 import com.yarukov.backend.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,15 +26,21 @@ public class AuthController {
     @Autowired
     private JwtUtils jwtUtils;
 
+    @Autowired
+    private MessageSource messageSource;
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody AuthRequest request) {
         boolean success = authService.register(request.getUsername(), request.getPassword());
+        String msg;
         if (success) {
+            msg = messageSource.getMessage("auth.register.succes",null, LocaleContextHolder.getLocale());
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(Map.of("message", "Пользователь успешно зарегистрирован"));
+                    .body(Map.of("message", msg));
         }
+        msg = messageSource.getMessage("auth.register.error",null,LocaleContextHolder.getLocale());
         return ResponseEntity.badRequest()
-                .body(Map.of("error", "Имя пользователя уже занято"));
+                .body(Map.of("error", msg));
     }
 
     @PostMapping("/login")
@@ -46,8 +54,9 @@ public class AuthController {
                     "username", user.getUsername()
             ));
         }
+        String msg =  messageSource.getMessage("auth.login.error",null,LocaleContextHolder.getLocale());
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("error", "Неверное имя пользователя или пароль"));
+                .body(Map.of("error", msg));
     }
 }
